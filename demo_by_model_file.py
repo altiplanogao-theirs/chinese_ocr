5b66a0c4875673a444eae6037a573f1be0269659
gaoyuan ai <altiplanogao@hotmail.com>
2018-07-06 00:00:43 +0800

    make change

---
+++ demo_by_model_file.py
@@ -0,0 +1,136 @@
+#-*- coding:utf-8 -*-
+import os
+import sys
+import cv2
+from math import *
+
+import keras
+import numpy as np
+from PIL import Image
+
+sys.path.append(os.getcwd() + '/ctpn')
+from ctpn.text_detect import text_detect
+from lib.fast_rcnn.config import cfg_from_file
+import time
+import shutil
+import numpy as np
+from PIL import Image
+from glob import glob
+image_files = glob('./test_images/*.*')
+
+
+from densenet.model import decode
+from densenet.densenet import normal_cnn
+
+
+import time
+def sort_box(box):
+    box = sorted(box, key=lambda x: sum([x[1], x[3], x[5], x[7]]))
+    return box
+def dumpRotateImage(img, degree, pt1, pt2, pt3, pt4):
+    height, width = img.shape[:2]
+    heightNew = int(width * fabs(sin(radians(degree))) + height * fabs(cos(radians(degree))))
+    widthNew = int(height * fabs(sin(radians(degree))) + width * fabs(cos(radians(degree))))
+    matRotation = cv2.getRotationMatrix2D((width // 2, height // 2), degree, 1)
+    matRotation[0, 2] += (widthNew - width) // 2
+    matRotation[1, 2] += (heightNew - height) // 2
+    imgRotation = cv2.warpAffine(img, matRotation, (widthNew, heightNew), borderValue=(255, 255, 255))
+    pt1 = list(pt1)
+    pt3 = list(pt3)
+
+    [[pt1[0]], [pt1[1]]] = np.dot(matRotation, np.array([[pt1[0]], [pt1[1]], [1]]))
+    [[pt3[0]], [pt3[1]]] = np.dot(matRotation, np.array([[pt3[0]], [pt3[1]], [1]]))
+    ydim, xdim = imgRotation.shape[:2]
+    imgOut = imgRotation[max(1, int(pt1[1])) : min(ydim - 1, int(pt3[1])), max(1, int(pt1[0])) : min(xdim - 1, int(pt3[0]))]
+
+    return imgOut
+
+# def decode(pred):
+#     char_list = []
+#     pred_text = pred.argmax(axis=2)[0]
+#     for i in range(len(pred_text)):
+#         if pred_text[i] != nclass - 1 and ((not (i > 0 and pred_text[i] == pred_text[i - 1])) or (i > 1 and pred_text[i] == pred_text[i - 2])):
+#             char_list.append(characters[pred_text[i]])
+#     return u''.join(char_list)
+
+def do_predict(rec_model, img):
+    width, height = img.size[0], img.size[1]
+    scale = height * 1.0 / 32
+    width = int(width / scale)
+
+    img = img.resize([width, 32], Image.ANTIALIAS)
+
+    '''
+    img_array = np.array(img.convert('1'))
+    boundary_array = np.concatenate((img_array[0, :], img_array[:, width - 1], img_array[31, :], img_array[:, 0]), axis=0)
+    if np.median(boundary_array) == 0:  # 将黑底白字转换为白底黑字
+        img = ImageOps.invert(img)
+    '''
+
+    img = np.array(img).astype(np.float32) / 255.0 - 0.5
+
+    X = img.reshape([1, 32, width, 1])
+
+    y_pred = rec_model.predict(X)
+    y_pred = y_pred[:, :, :]
+
+    # out = K.get_value(K.ctc_decode(y_pred, input_length=np.ones(y_pred.shape[0]) * y_pred.shape[1])[0][0])[:, :]
+    # out = u''.join([characters[x] for x in out[0]])
+    out = decode(y_pred)
+
+    return out
+
+
+def charRec(rec_model, img, text_recs, adjust=False):
+    """
+    加载OCR模型，进行字符识别
+    """
+    results = {}
+    xDim, yDim = img.shape[1], img.shape[0]
+
+    for index, rec in enumerate(text_recs):
+        xlength = int((rec[6] - rec[0]) * 0.1)
+        ylength = int((rec[7] - rec[1]) * 0.2)
+        if adjust:
+            pt1 = (max(1, rec[0] - xlength), max(1, rec[1] - ylength))
+            pt2 = (rec[2], rec[3])
+            pt3 = (min(rec[6] + xlength, xDim - 2), min(yDim - 2, rec[7] + ylength))
+            pt4 = (rec[4], rec[5])
+        else:
+            pt1 = (max(1, rec[0]), max(1, rec[1]))
+            pt2 = (rec[2], rec[3])
+            pt3 = (min(rec[6], xDim - 2), min(yDim - 2, rec[7]))
+            pt4 = (rec[4], rec[5])
+
+        degree = degrees(atan2(pt2[1] - pt1[1], pt2[0] - pt1[0]))  # 图像倾斜角度
+
+        partImg = dumpRotateImage(img, degree, pt1, pt2, pt3, pt4)
+
+        if partImg.shape[0] < 1 or partImg.shape[1] < 1 or partImg.shape[0] > partImg.shape[1]:  # 过滤异常图片
+            continue
+
+        image = Image.fromarray(partImg).convert('L')
+        text = do_predict(rec_model, image)
+
+        if len(text) > 0:
+            results[index] = [rec]
+            results[index].append(text)  # 识别文字
+
+    return results
+if __name__ == '__main__':
+    result_dir = './test_result'
+    if os.path.exists(result_dir):
+        shutil.rmtree(result_dir)
+    os.mkdir(result_dir)
+
+    # rec_model =
+
+    rec_model = keras.models.load_model('/workzone/tmp/basemodel.h5')
+
+    for image_file in sorted(image_files):
+        image = np.array(Image.open(image_file).convert('RGB'))
+        t = time.time()
+        text_recs, img_framed, image = text_detect(image)
+        text_recs = sort_box(text_recs)
+        result = charRec(rec_model, image, text_recs, adjust=False)
+        pass
